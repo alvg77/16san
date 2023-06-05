281c301fd7882d5e99298d3f644cd9cef903bcfc
Aleko Georgiev <61247544+unamusedchemical@users.noreply.github.com>
2023-06-06 00:19:03 +0300

    bans

---
+++ src/main/java/com/ssan/api16san/controller/advice/UserBanAdvice.java
@@ -0,0 +1,15 @@
+package com.ssan.api16san.controller.advice;
+
+import com.ssan.api16san.exceptions.UnauthorizedException;
+import com.ssan.api16san.exceptions.UserBanException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+public class UserBanAdvice extends ResponseEntityExceptionHandler {
+    @ExceptionHandler(UserBanException.class)
+    public ResponseEntity<?> handleUnauthorized(UserBanException ex) {
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(ex.getMessage());
+    }
+}
